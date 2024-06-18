6e9adc0a3a9d318557865311d0c04df47fc62303
ilMike42 <ilmike_others@icloud.com>
2024-06-18 22:34:47 +0200

    added daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/routes/**/*.{svelte,js,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  }
 }
 

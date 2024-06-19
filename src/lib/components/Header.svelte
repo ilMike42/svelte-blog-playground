a8478285b37b09961ce3a432b37dfbc225b824a8
ilMike42 <ilmike_others@icloud.com>
2024-06-19 19:56:10 +0200

    created Header component (placeholder)

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Menu from "lucide-svelte/icons/menu";
+	import Button from "./ui/button/button.svelte";
+	import Search  from "lucide-svelte/icons/search";
+
+</script>
+<div class="navbar bg-base-100 mb-40 shadow-xl rounded-box">
+	<div class="navbar-start">
+
+		<Button variant="ghost" size="circle">
+			<Menu />
+		</Button>
+	</div>
+	<div class="navbar-center">
+		<!-- <a class="btn btn-ghost text-xl">daisyUI</a> -->
+	</div>
+	<div class="navbar-end">
+		 <Button variant="ghost" size="circle">
+			<Search />
+		</Button>
+	</div>
+</div>

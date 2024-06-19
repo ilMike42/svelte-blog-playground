5cfc57b871f665dd390644d8557a90ff3d74490b
ilMike42 <ilmike_others@icloud.com>
2024-06-19 19:50:54 +0200

    added buttons

---
+++ src/lib/components/ui/button/index.ts
@@ -0,0 +1,50 @@
+import { type VariantProps, tv } from "tailwind-variants";
+import type { Button as ButtonPrimitive } from "bits-ui";
+import Root from "./button.svelte";
+
+const buttonVariants = tv({
+	base: "btn",
+	variants: {
+		variant: {
+			default: "",
+			secondary: "btn-secondary",
+			neutral: "btn-neutral",
+			primary: "btn-primary",
+			accent: "btn-accent",
+			ghost: "btn-ghost",
+			link: "btn-link",
+		},
+		size: {
+			default: "h-10 px-4 py-2",
+			sm: "h-9 rounded-md px-3",
+			lg: "h-11 rounded-md px-8",
+			icon: "h-10 w-10",
+			circle: "btn-circle"
+		},
+	},
+	defaultVariants: {
+		variant: "default",
+		size: "default",
+	},
+});
+
+type Variant = VariantProps<typeof buttonVariants>["variant"];
+type Size = VariantProps<typeof buttonVariants>["size"];
+
+type Props = ButtonPrimitive.Props & {
+	variant?: Variant;
+	size?: Size;
+};
+
+type Events = ButtonPrimitive.Events;
+
+export {
+	Root,
+	type Props,
+	type Events,
+	//
+	Root as Button,
+	type Props as ButtonProps,
+	type Events as ButtonEvents,
+	buttonVariants,
+};

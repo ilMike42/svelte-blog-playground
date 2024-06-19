59b6845000bf405fa2387298ad6821d56a8399ae
ilMike42 <ilmike_others@icloud.com>
2024-06-19 20:20:46 +0200

    added class compatibility to header and footer components

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,14 @@
-<script>
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
 
+	type $$Props = HTMLInputAttributes;
+
+	let className: $$Props['class'] = undefined;
+
+	export { className as class };
 </script>
 
-<div>
-
+<div class={cn(className)}>
+    <slot />
 </div>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,12 +1,20 @@
-<script>
-	import Menu from "lucide-svelte/icons/menu";
-	import Button from "./ui/button/button.svelte";
-	import Search  from "lucide-svelte/icons/search";
+<script lang="ts">
+	import Menu from 'lucide-svelte/icons/menu';
+	import Button from './ui/button/button.svelte';
+	import Search from 'lucide-svelte/icons/search';
 
+	import type { HTMLInputAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
+
+	type $$Props = HTMLInputAttributes;
+
+	let className: $$Props['class'] = undefined;
+
+	export { className as class };
 </script>
-<div class="navbar bg-base-100 mb-40 shadow-xl rounded-box">
-	<div class="navbar-start">
 
+<div class={cn("navbar bg-base-100 mb-40 shadow-xl rounded-box", className)}>
+	<div class="navbar-start">
 		<Button variant="ghost" size="circle">
 			<Menu />
 		</Button>
@@ -15,7 +23,7 @@
 		<!-- <a class="btn btn-ghost text-xl">daisyUI</a> -->
 	</div>
 	<div class="navbar-end">
-		 <Button variant="ghost" size="circle">
+		<Button variant="ghost" size="circle">
 			<Search />
 		</Button>
 	</div>

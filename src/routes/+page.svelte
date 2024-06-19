0293bc5bc0e26bbff541c44c3a4ada92cbf0f56d
ilMike42 <ilmike_others@icloud.com>
2024-06-19 20:40:27 +0200

    populated demo section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-Homepage
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+
+<Button href="/demo" variant="primary"> GO TO DEMO...</Button>
--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -1,3 +1,18 @@
-<div>
-    PAGINA DEMO
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<div class="mx-12">
+	<article class="prose">
+		<h1>Buttons</h1>
+        <div class="flex gap-5">
+            <Button>Default</Button>
+            <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="accent">Accent</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="link">Link</Button>
+            <Button variant="neutral">Neutral</Button>
+        </div>
+	</article>
 </div>
